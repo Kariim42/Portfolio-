202666fd6de1599b2625ce889fa99cd2e1be42cd
Kariim42 <minouche42152@gmail.com>
2022-09-28 23:08:34 +0200

    fix problem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,7 @@ import { ProjectComponent } from './project/project.component';
     ProjectComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule
   ],
   providers: [],
